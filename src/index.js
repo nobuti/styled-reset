2ad47e936bc5a5ffa1edd3ce391fcb9094d58298
nobuti <buti@nobuti.com>
2019-10-24 09:24:51 +0200

    Modern css reset on top of styled-components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,90 @@
-import hello from './modules/hello'
+import { css, createGlobalStyle } from 'styled-components'
 
-export { hello }
+export const reset = css`
+/* Box sizing rules */
+html {
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after { 
+  box-sizing: inherit;
+}
+
+/* Remove default padding and list-styles */
+ul,
+ol {
+  padding: 0;
+  list-style: none;
+}
+
+/* Remove default margin */
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+ul,
+ol,
+li,
+figure,
+figcaption,
+blockquote,
+dl,
+dd {
+  margin: 0;
+}
+
+/* Set core body defaults */
+body {
+  min-height: 100vh;
+  scroll-behavior: smooth;
+  text-rendering: optimizeSpeed;
+  line-height: 1.4;
+}
+
+/* Make images easier to work with */
+img,
+video {
+  height: auto;
+  max-width: 100%;
+  display: block;
+}
+
+/* Inherit fonts for inputs and buttons */
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
+/* Reset iframe border */
+iframe {
+  border: 0;
+}
+
+/* Table modern styles */
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+td,
+th {
+  padding: 0;
+}
+
+td:not([align]),
+th:not([align]) {
+  text-align: left;
+}
+`
+
+export const Reset = createGlobalStyle`${reset}`
+
+export default reset
